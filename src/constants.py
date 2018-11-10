cdf64c44b03ada73aca08001ddd197c74a3d971f
Alex <lmntrix.afx@gmail.com>
2018-11-10 23:53:23 +0330

    init constracts file

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+# all reserved keywords place here!
+
+# TODO add instagram sutiable datatypes and keywords
+# like [user,password,
+
+KEYWORDS = ["if", "for", "else", "True", "False", "print", "bool", "int", "str"]
+BUILT_IN_FUNCS = ["print"]
+DATA_TYPE = ["int", "str", "bool"]
