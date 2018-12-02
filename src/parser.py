f9b3d83f0392685ad235f55170eeec9a17248544
Alex <lmntrix.afx@gmail.com>
2018-12-02 12:55:17 +0330

    added get_token_to_the_matcher function that get all tokens till find the correct matcher

--- src/parser.py
+++ src/parser.py
@@ -16,3 +16,28 @@ class Parser(object):
         # This will hold the token index we are parsing at right now
         self.token_index = 0
 
+    def get_token_to_the_matcher(self, matcher, terminating_matcher, token_stream):
+
+        # This will get all the tokens in a token stream until it find the token with the correct
+        # matcher
+        # return  A list of all the tokens found before the matcher
+
+        # keep all tokens here
+        tokens = []
+        # index of checked tokens
+        tokens_checked = 0
+
+        for token in token_stream:
+            tokens_checked += 1  # increase index
+            # If the terminating matcher is found then return False
+            # as it means scope we allow for the check is reached
+            if token[1] == terminating_matcher:
+                return False
+            # If the token matcher is found then return all the tokens found before it or else append the tokens to var
+            if token[1] == matcher:
+                return [tokens, tokens_checked - 1]
+            else:
+                tokens.append(token)
+
+        # Return False if the matcher or the terminator_matcher is found
+        return False
