e8f07da5b6775b2efa4eb0d73afd37b51e4a66a4
Alex <lmntrix.afx@gmail.com>
2018-12-17 09:11:55 +0330

    start objects

--- src/Objects/varObject.py
+++ src/Objects/varObject.py
@@ -1,8 +1,29 @@
 class VariableObject(object):
 
-    def __init__(self):
+    def __init__(self, ast):
+        # The ast will hold the dictionary version of the ast which is like a blueprint
+        self.ast = ast['VariableDecleration']
+        # This will hold the exec string for variable decleration
         self.exec_string = ""
 
-    def transpile(self, name, operator, value):
-        self.exec_string += name + " " + operator + " " + value+"\n"
+    def transpile(self):
+
+        # This method will use the AST in order to create a python version of the gleem
+        # generated dictionary AST.
+
+        # Loop through all dictionary values
+        for value in self.ast:
+
+            # Get the name of the variable
+            try:
+                self.exec_string += value['name'] + " = "
+            except:
+                pass
+
+            # Get the value of the variable
+            try:
+                self.exec_string += str(value['value'])
+            except:
+                pass
+
         return self.exec_string
