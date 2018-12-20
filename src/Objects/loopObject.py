dcbae2b98d4d5241161dd73fea27121f8f52c4b3
Alex <lmntrix.afx@gmail.com>
2018-12-20 15:55:39 +0330

    loop object is done!

--- src/Objects/loopObject.py
+++ src/Objects/loopObject.py
@@ -139,3 +139,83 @@ class LoopObject(object):
                     body_exec_string += ("   " * (nesting_count - 1)) + loop_obj.translate()
 
         return body_exec_string
+
+    def check_ast(self, astName, ast):
+        # check for that ast be not empty
+        try:
+            if ast[astName] is []:
+                return True
+            if ast[astName]:
+                return True
+        except Exception:
+            return False
+
+    def should_dedent_trailing(self, ast, full_ast):
+
+        # This method will check if the ast item being checked is outside a conditional statement e.g.
+
+        # if a == 11 {
+        #     if name == "Alex" {
+        #         print "Not it";
+        #     }
+        #     print "Hi"; <--- This is the code that should be indented by 1
+        #     so when found will return true if dedent flag is true
+        # }
+        # will return  ==> True  : If the code should not be indented
+        # because it is in current scope below current nested condition
+        #             ==> False : The item should not be indented
+
+        # This  will creates an array of only body elements
+        new_just_body_ast = full_ast[len(full_ast) - 1]['body']
+        # This will know whether it should dedent
+        dedent_flag = False
+
+        # Loop through body's ast and when a conditional statement is founds set
+        # the dedent flag to 'true'
+        for x in new_just_body_ast:
+
+            # When a conditional stateemenet AST is found set the dedent trailing to true
+            if self.check_ast('ForLoop', x):
+                dedent_flag = True
+
+            if ast == x and dedent_flag is True:
+                return True
+
+        return False
+
+    def should_increment_nest_count(self, ast, full_ast):
+
+        # This method will check if another statement is found and whether or not it should increase
+        # nesting count for example
+        # if a == 11 {
+        #     if name == "Alex" {
+        #         print "Not it";
+        #     }
+        #     if 1 != 2 { <--- This is the statement that should not be nested more
+        #         print "Yo man! "
+        #     }
+        # }
+        # will return:
+        #     True  : If the nesting should increase by 1
+        #     False : If the nesting should not be increased
+
+        # Counts of the number of statements in that one scope
+        statement_counts = 0
+
+        # Loops through the body to count the number of conditional statements
+        for x in full_ast[len(full_ast) - 1]['body']:
+
+            # If a statement is found then increment statement count variable value by 1
+            if self.check_ast('ForLoop', x):
+                statement_counts += 1
+
+            # If the statement being checked is the one found then break
+            if ast == x:
+                break
+
+        # Return false if there were less then 1 statements
+        if statement_counts > 1:
+            return False
+        # Return true if there were more than 1 statements
+        else:
+            return True
