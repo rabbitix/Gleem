a61134222b3f9c4c7b77ce35fa53e3afe065626a
Alex <lmntrix.afx@gmail.com>
2018-12-30 16:24:50 +0330

    added main part..instagram library and make a class of it
    but it still need some extra works

--- tryinsta/objgen.py
+++ tryinsta/objgen.py
@@ -1,7 +1,29 @@
 import constant
 
+from InstagramAPI import InstagramAPI
+import time
 
-class Comp():
+
+class IGram:
+    def __init__(self, username=None, password=None):
+        if username:
+            self.ig = InstagramAPI(username, password)
+            self.ig.login()
+        else:
+            pass
+
+    def start(self):
+        pass
+
+    def like_last_post(self, username=None):
+        self.ig.searchUsername(username)
+        user_id = self.ig.LastJson['user']['pk']
+        user_posts = self.ig.getTotalUserFeed(usernameId=user_id)
+        last_media_id = user_posts[0]['id']
+        self.ig.like(last_media_id)
+
+
+class Comp(object):
     def __init__(self):
         # self.exe_string = ""
         self.values = ""
@@ -9,3 +31,23 @@ class Comp():
     def translate(self, token_type=None, operation=None, value=None):
         self.values += value + ' '
         return self.values
+
+    def compile(self, values):
+        all = []
+        username = ""
+        password = ""
+
+        all = values.split()
+        target_username = ""
+
+        # if all[0] is "User":
+        username = all[1]
+        # if all[2] is "Password":
+        password = all[3]
+        # if all[4] is "Start":
+        # obj = IGram(username, password)
+        # if all[5] is "like_last_post":
+        target_username = all[6]
+        # obj.like_last_post(username=target_username)
+
+        print('s')
