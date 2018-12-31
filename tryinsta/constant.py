5b738f089f3ae1e853c4b89979b6581110776cb7
Alex <lmntrix.afx@gmail.com>
2018-12-31 09:56:19 +0330

    made constants file a dictionary
    so much better!!

--- tryinsta/constant.py
+++ tryinsta/constant.py
@@ -1,5 +1,14 @@
-KEYWORDS = ["User", "Password", "Start"]
-DATATYPE = ["LikeLastPost", "like_last_post", "Follow", "Like"]
+KEYWORDS = {
+    "UserIdentifier": ["User", "user"],
+    "PasswordIdentifier": ["Password", "password"],
+    "StartIdentifier": ["Start", "start"],
+}
 
-# TODO make it dict
-#TODO make it non case sensetive
+DATATYPE = {
+    "likeLastPostIdentifier": ["LikeLastPost", "like_last_post"],
+    "FollowIdentifier": ["Follow"],
+    "LikeIdentifier": ["Like"],
+}
+
+
+# TODO make it non case sensetive
