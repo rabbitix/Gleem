c092bb38854dda4de4de24c81bc5eab4034ad61f
Alex <lmntrix.afx@gmail.com>
2018-12-31 10:41:19 +0330

    separate instagram part totally

---
+++ tryinsta/instagram.py
@@ -0,0 +1,21 @@
+from InstagramAPI import InstagramAPI
+import time
+
+
+class IGram:  # class for instagram part
+    def __init__(self, username=None, password=None):
+        if username:
+            self.ig = InstagramAPI(username, password)
+            self.ig.login()
+        else:
+            pass
+
+    def start(self):
+        pass
+
+    def like_last_post(self, username=None):
+        self.ig.searchUsername(username)
+        user_id = self.ig.LastJson['user']['pk']
+        user_posts = self.ig.getTotalUserFeed(usernameId=user_id)
+        last_media_id = user_posts[0]['id']
+        self.ig.like(last_media_id)
