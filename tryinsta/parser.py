7dc08e6da8650da6122b43cb73a7f928316a127c
Alex <lmntrix.afx@gmail.com>
2019-01-01 22:42:19 +0330

    purple for parser part added

--- tryinsta/parser.py
+++ tryinsta/parser.py
@@ -13,7 +13,7 @@ class Parser(object):
         while self.token_index < len(self.tokens):
             token_type = self.tokens[self.token_index][0]
             token_value = self.tokens[self.token_index][1]
-            print('----------------------------[ ' + token_type, token_value + ' ]----------------------------')
+            print('\033[35m----------------------------[ ' + token_type, token_value + ' ]----------------------------')
             self.all_tokens = com.translate(value=token_value)
             self.token_index += 1
 
