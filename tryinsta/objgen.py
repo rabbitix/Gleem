d112c2b615f648d21ef09ac7ec53ba39a9ea8cc5
Alex <lmntrix.afx@gmail.com>
2019-01-02 00:54:42 +0330

    make a little change that make a little optimize in loop trough!!

--- tryinsta/objgen.py
+++ tryinsta/objgen.py
@@ -12,32 +12,32 @@ class Comp(object):
 
     def show_error(self, msgtxt=None):
         print("\033[33m============ ERROR ============\033[0m")
-        print('\033[31m' + msgtxt )
+        print('\033[31m' + msgtxt)
         print("\033[33m===============================\033[0m")
 
     def compile(self, values):
-        all = []
+        all_vals = []
         username = ""
         password = ""
-        all = values.split()
+        all_vals = values.split()
         # check that start of the file works well! and is what we want
-        if all[0] in KEYWORDS["UserIdentifier"] and \
-                all[2] in KEYWORDS["PasswordIdentifier"] and \
-                all[4] in KEYWORDS["StartIdentifier"]:
+        if all_vals[0] in KEYWORDS["UserIdentifier"] and \
+                all_vals[2] in KEYWORDS["PasswordIdentifier"] and \
+                all_vals[4] in KEYWORDS["StartIdentifier"]:
             # check for `user` keyword
-            if all[0] in KEYWORDS["UserIdentifier"]:
-                username = all[1]
+            if all_vals[0] in KEYWORDS["UserIdentifier"]:
+                username = all_vals[1]
             else:
                 self.show_error("your syntax should start with init the user")
                 quit()
             # check for `password` keyword
-            if all[2] in KEYWORDS["PasswordIdentifier"]:
-                password = all[3]
+            if all_vals[2] in KEYWORDS["PasswordIdentifier"]:
+                password = all_vals[3]
             else:
                 self.show_error("after identifying user, you should pass the password! ")
                 quit()
             # check for `start` keyword
-            if all[4] in KEYWORDS["StartIdentifier"]:
+            if all_vals[4] in KEYWORDS["StartIdentifier"]:
                 print("should start here!")
                 # igobj = instagram.IGram(username, password)
             else:
@@ -47,15 +47,19 @@ class Comp(object):
             self.show_error("you didnt use the correct structure for the start of the syntax")
             quit()
         index = 5
-        while index < len(all):
-            if all[index] in DATATYPE["likeLastPostIdentifier"]:
+        while index < len(all_vals):
+            if all_vals[index] in DATATYPE["likeLastPostIdentifier"]:
                 print("should do like last post function ")
-                # igobj.like_last_post(all[index+1])
-            if all[index] in DATATYPE["FollowIdentifier"]:
-                print("should do follow function")
-                # igobj.follow_user(all[index+1])
+                # igobj.like_last_post(all_vals[index+1])
+                index += 2
 
-            # if all[index] in DATATYPE["LikeIdentifier"]:
+            elif all_vals[index] in DATATYPE["FollowIdentifier"]:
+                print("should do follow function")
+                index += 2
+                # igobj.follow_user(all_vals[index+1])
+
+            # if all_vals[index] in DATATYPE["LikeIdentifier"]:
             #     print(" should do liking function")
 
-            index += 1
+            else:
+                index += 1
